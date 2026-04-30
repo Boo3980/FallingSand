b339f27422f20463b3dfffc84c2390bdc938d73f
Pratham Bhanshali <prathambhanshali1@gmail.com>
2026-05-01 01:39:23 +0530

    bruh

--- Main.cpp
+++ Main.cpp
@@ -122,7 +122,7 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
 	
 	// now we give the buffers access to the data
-	glBufferData(vertexBufferObject, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(vertexArrayObject, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	
 

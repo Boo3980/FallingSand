9fa3849fc6006d986a800602c198c0347e070e98
Pratham Bhanshali <prathambhanshali1@gmail.com>
2026-05-01 02:58:36 +0530

    I GOT IT

--- Main.cpp
+++ Main.cpp
@@ -73,12 +73,12 @@ int main() {
 	// time to re-code the entire day 2 thing AND more
 
 	GLfloat vertices[] = {
-		-0.5f ,-0.5f*float(sqrt(3))     / 3, 0.0f,                          // for OpenGL the coordinates are normalised. leftmost for x is -1.0f 
-		 0.5f ,-0.5f*float(sqrt(3))     / 3, 0.0f,
-		 0.0f , 0.5f*float(sqrt(3)) * 2 / 3, 0.0f,						   // right most is 1.0f. likewise with y-coordinates.
-		-0.5f/2, 0.5f*float(sqrt(3))     / 6, 0.0f,
-		 0.5f/2, 0.5f*float(sqrt(3))     / 6, 0.0f,
-		 0.0f ,-0.5f*float(sqrt(3))      /3 , 0.0f 
+		-0.5f   ,-0.5f * float(sqrt(3))     / 3, 0.0f,                          // for OpenGL the coordinates are normalised. leftmost for x is -1.0f 
+		 0.5f   ,-0.5f * float(sqrt(3))     / 3, 0.0f,
+		 0.0f   , 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,						   // right most is 1.0f. likewise with y-coordinates.
+		-0.5f/2 , 0.5f * float(sqrt(3))     / 6, 0.0f,
+		 0.5f/2 , 0.5f * float(sqrt(3))     / 6, 0.0f,
+		 0.0f   ,-0.5f * float(sqrt(3))     / 3 ,0.0f 
 	};
 
 	GLuint indices[] = {
@@ -135,9 +135,10 @@ int main() {
 	// now release the bind 
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
 
-	glBindVertexArray(0);
+	
 
 	float r = 88.0f;//
 	float g = 25.0f;// rgb-->(0-255)
